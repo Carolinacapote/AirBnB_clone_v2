ad160be052ab7ca7e859e23124ab8cf1ec3d5d03
Carolinacapote <carolina.capote28@gmail.com>
2021-08-26 15:34:00 +0000

    [FEATURE]Generate a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder
+'''
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''Function that generates the .tgz archives '''
+    # if versions folder does not exist
+    local('mkdir -p versions/')
+
+    date_time = datetime.now()
+    str_date = date_time.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + str_date + '.tgz'
+
+    # Generates the .tgz archive
+    try:
+        tar_file = local('tar -cvzf {} web_static/'.format(archive))
+        return archive_path
+    except:
+        return None
