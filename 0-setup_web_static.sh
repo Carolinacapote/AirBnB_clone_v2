b32089b93fbe87efdd5edfd70cfc342bf1bb6054
Carolinacapote <carolina.capote28@gmail.com>
2021-08-26 20:40:25 +0000

    [FIX]Super user access

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,20 +2,20 @@
 # Bash script that sets up your web servers for the deployment of web_static.
 
 # Install nginx
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 # Create the new folders if they do not exist
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
 # Create a fake HTML file to test the Nginx configuration
 echo 'Holberton School Test' > /data/web_static/releases/test/index.html
 # Create a symbolic link to the /data/web_static/releases/test/ folder
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sed -i '/listen 80 default_server;/a location /hbtn_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a location /hbtn_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 # Restart nginx
-service nginx restart
+sudo service nginx restart
 exit 0
