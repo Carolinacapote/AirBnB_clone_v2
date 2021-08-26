79f668c215013d8ada154f6f671722512e0b754d
Carolinacapote <carolina.capote28@gmail.com>
2021-08-26 16:01:33 +0000

    [FIX]Format

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 # Create a fake HTML file to test the Nginx configuration
-echo 'Holberton School'> /data/web_static/releases/test/index.html
+echo 'Holberton School' > /data/web_static/releases/test/index.html
 # Create a symbolic link to the /data/web_static/releases/test/ folder
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group
