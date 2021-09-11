1cfd0879da694182df2c46b5c2e168510b890165
Carolinacapote <carolina.capote28@gmail.com>
2021-09-11 20:23:37 +0000

    [FIX]Change return value

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    return 'Hello HBNB!'
+    """Return a string as a content"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB!'
+    """Return a string as a content"""
+    return "HBNB"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
