83670a8e363d0a42269763f8e273e5be1c9f11c5
Carolinacapote <carolina.capote28@gmail.com>
2021-09-11 22:05:09 +0000

    [ADD]metohd is_int

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,30 +8,36 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """Return a string as a content"""
     return 'Hello HBNB!'
     
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB!'
+    """Return a string as a content"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Return a string as a content"""
     text = text.replace('_', ' ')
-    return 'C' + '%s' % text
+    return 'C ' + text
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
-def python_text(text):
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Return a string as a content"""
     text = text.replace('_', ' ')
-    return 'C' + '%s' % text
+    return 'Python ' + text
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return 'C' + '%s' % text
+@app.route('/number/<n>', strict_slashes=False)
+def is_int(text):
+    """Return a string as a content"""
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
