f199bf5dc4b88d9423d38eb4865b0bf408c43c4b
Carolinacapote <carolina.capote28@gmail.com>
2021-09-12 23:06:04 +0000

    [ADD]Display states + cities per state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def index():
+    """Displays a list of all states with a description"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
